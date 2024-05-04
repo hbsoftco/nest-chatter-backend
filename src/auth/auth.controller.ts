e0bc303f5c7de8fb3a57bea1511cc4a2bb664a72
Hossein <hossein.bajan@gmail.com>
2024-05-04 14:30:21 +0330

    Add login service to create token payload by jwt, create token payload interface and use service in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,18 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { CurrentUser } from './current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { Response } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) response: Response,
   ) {
-    console.log(user);
-    console.log(response);
+    return this.authService.login(user, response);
   }
 }
